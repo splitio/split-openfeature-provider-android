3e88c2cc7638fe5babf77f8cd32528c6de6879e9
gthea <gaston.thea@harness.io>
2025-09-16 10:57:33 -0300

    Project setup (#2)

---
+++ src/test/java/io/split/openfeature/android/provider/SplitProviderTest.kt
@@ -0,0 +1,13 @@
+package io.split.openfeature.android.provider
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SplitProviderTest {
+
+    @Test
+    fun `name is Split`() {
+        assertEquals("Split", SplitProvider.NAME)
+    }
+}
