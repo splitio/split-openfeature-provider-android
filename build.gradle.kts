ab9de17cc2196d2abe480620796377e1935b56f4
gthea <gaston.thea@harness.io>
2025-10-01 18:02:36 -0300

    Release 1.0.0 (#16)

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
 }
 
-val providerVersion = "1.0.0-rc2"
+val providerVersion = "1.0.0"
 
 val splitPOM = Action<MavenPom> {
     name.set("Split OpenFeature Provider for Android")
